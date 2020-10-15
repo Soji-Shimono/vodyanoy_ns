c56171f9594e8098c167fccbfc306dac6e0bc1f3
soji shimono <shimon0125@yahoo.co.jp>
2020-10-15 12:36:39 +0900

    Command calculation to the thruster driver is changed. Limitation of the command value is added.

--- src/can_driver/scripts/canDriver_ns.py
+++ src/can_driver/scripts/canDriver_ns.py
@@ -19,43 +19,46 @@ depthmsg = Point()
 # 32768 * 0.2 = 6553.6
 dT = 0
 DEADBAND = 1
+MAX_COMMAND = 10000
+
 def callback(message):
     mode = 0
     if message.mode == "rpm":
         for i in range():
-            b2 = struct.pack('>h',int(message.Thruster1.rpm * 5))
-            b3 = struct.pack('>h',int(message.Thruster2.rpm * 5))
-            b6 = struct.pack('>h',int(message.Thruster3.rpm * 5))
-            b5 = struct.pack('>h',int(message.Thruster4.rpm * 5))
-            b4 = struct.pack('>h',int(message.Thruster5.rpm * 5))
-            b1 = struct.pack('>h',int(message.Thruster6.rpm * 5))
+            _b2 = rpm2com(message.Thruster1.rpm,MAX_COMMAND,DEADBAND)
+            _b3 = rpm2com(message.Thruster2.rpm,MAX_COMMAND,DEADBAND)
+            _b6 = rpm2com(message.Thruster3.rpm,MAX_COMMAND,DEADBAND)
+            _b5 = rpm2com(message.Thruster4.rpm,MAX_COMMAND,DEADBAND)
+            _b4 = rpm2com(message.Thruster5.rpm,MAX_COMMAND,DEADBAND)
+            _b1 = rpm2com(message.Thruster6.rpm,MAX_COMMAND,DEADBAND)
+
             print("mode1")
             mode = 1
     if message.mode == "rate":
-            b2 = struct.pack('>h',checkDeadband_rate(message.Thruster1.parsentage * -1.0,DEADBAND))
-            b3 = struct.pack('>h',checkDeadband_rate(message.Thruster2.parsentage * -1.0,DEADBAND))
-            b6 = struct.pack('>h',checkDeadband_rate(message.Thruster3.parsentage * -1.0,DEADBAND))
-            b5 = struct.pack('>h',checkDeadband_rate(message.Thruster4.parsentage,DEADBAND))
-            b4 = struct.pack('>h',checkDeadband_rate(message.Thruster5.parsentage,DEADBAND))
-            b1 = struct.pack('>h',checkDeadband_rate(message.Thruster6.parsentage,DEADBAND))
+            _b2 = rate2com(message.Thruster1.parsentage * -1.0,MAX_COMMAND,DEADBAND)
+            _b3 = rate2com(message.Thruster2.parsentage * -1.0,MAX_COMMAND,DEADBAND)
+            _b6 = rate2com(message.Thruster3.parsentage * -1.0,MAX_COMMAND,DEADBAND)
+            _b5 = rate2com(message.Thruster4.parsentage,MAX_COMMAND,DEADBAND)
+            _b4 = rate2com(message.Thruster5.parsentage,MAX_COMMAND,DEADBAND)
+            _b1 = rate2com(message.Thruster6.parsentage,MAX_COMMAND,DEADBAND)
+
             mode = 2
             print("mode2")
     else:
-            '''
-            b1 = struct.pack('>h',0)
-            b2 = struct.pack('>h',0)
-            b3 = struct.pack('>h',0)
-            b4 = struct.pack('>h',0)
-            b5 = struct.pack('>h',0)
-            b6 = struct.pack('>h',0)
-            '''
-            b1 = struct.pack('>h',int(message.Thruster1.rpm * 5))
-            b2 = struct.pack('>h',int(message.Thruster2.rpm * 5))
-            b3 = struct.pack('>h',int(message.Thruster3.rpm * 5))
-            b4 = struct.pack('>h',int(message.Thruster4.rpm * 5))
-            b5 = struct.pack('>h',int(message.Thruster5.rpm * 5))
-            b6 = struct.pack('>h',int(message.Thruster6.rpm * 5))
+            _b1 = int(message.Thruster1.rpm * 5)
+            _b2 = int(message.Thruster2.rpm * 5)
+            _b3 = int(message.Thruster3.rpm * 5)
+            _b4 = int(message.Thruster4.rpm * 5)
+            _b5 = int(message.Thruster5.rpm * 5)
+            _b6 = int(message.Thruster6.rpm * 5)
             print("mode_")
+    
+    b1 = struct.pack('>h',_b1)
+    b2 = struct.pack('>h',_b2)
+    b3 = struct.pack('>h',_b3)
+    b4 = struct.pack('>h',_b4)
+    b5 = struct.pack('>h',_b5)
+    b6 = struct.pack('>h',_b6)
 
     msg = can.Message(arbitration_id=0x73, dlc=1, data=[mode],extended_id=False)
     #print(msg)
@@ -71,11 +74,21 @@ def callback(message):
 	#can.Message(timestamp=0.0, arbitration_id=0, is_extended_id=None, is_remote_frame=False, 
 	# is_error_frame=False, channel=None, dlc=None, data=None, is_fd=False, bitrate_switch=False, 
 	# error_state_indicator=False, extended_id=True, check=False)
-def checkDeadband_rate(a,band):
-    t = int(a * 32767 / 100)
+
+def rate2com(rate,LIMIT,band):
+    t = int(rate / 100 * LIMIT)
     if abs(t) < band:
         t = 0
     return t
+
+def rpm2com(rpm,LIMIT,band):
+    t = int(rpm * 5)
+    if abs(t) < band:
+        t = 0
+    if abs(t) > LIMIT:
+        t = LIMIT * t / abs(t)
+    return int(t)
+
 def int_from_bytes(b):
     '''Convert big-endian signed integer bytearray to int
     int_from_bytes(b) == int.from_bytes(b, 'big', signed=True)'''
